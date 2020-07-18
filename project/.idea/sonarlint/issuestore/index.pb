61a50bd557328e561a09c646fd6ba7fdc1d0bab1
EvgenyChernov <evgenycher89@gmail.com>
2020-07-18 13:06:18 +0300

    Задание первого урока выполнено v_01

---
+++ project/.idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,11 @@
+
+:
+
+js/main.js,d\a\dae13fd6f331d1c91ab58b06e81d05cb8958bfb1
+:
+
+index.html,f\6\f6013a00b362253c64368d6eebc50ea2131754e2
+D
+styles/my-style.sass,a\3\a3b6854971f42628c33f001f2aade2cedf73e55f
+C
+styles/my-style.css,7\4\7488f1b51dd5adde4d2948e82b1053a8a06302a8
--- project/js/main.js
+++ project/js/main.js
@@ -5,19 +5,33 @@ const products = [
     {id: 4, title: 'Gamepad', price: 4500},
 ];
 
-const renderProduct = (title, price, img='') => {
+/**
+ * Функция свойства объекта  преобразует их и возвращает строку
+ * @param title название товара
+ * @param price цена товара
+ * @param img картинка если есть
+ * @return {string} часть кода HTML (для одного продукта)
+ */
+const renderProduct = (title, price, img = 'http://dummyimage.com/260x350/99cccc.jpg/0b0b0b&text=Фото+товара!') => {
     return `<div class="product-item">
                 <h3>${title}</h3>
                 <img src="${img}" alt="">
-                <p>${price}</p>
+                <p>Цена ${price}</p>
                 <button class="by-btn">Добавить в корзину</button>
               </div>`;
 };
 
+/**
+ * Функция создает карточки продукта
+ * @param list
+ */
 const renderProducts = list => {
     const productList = list.map(item => renderProduct(item.title, item.price));
-    console.log(productList);
-    document.querySelector('.products').innerHTML = productList;
+    for (let i = 0; i < productList.length; i++) {
+        document.querySelector('.products').innerHTML += productList[i];
+    }
+    // console.log(productList);
+    // document.querySelector('.products').innerHTML = productList;
 };
 
 renderProducts(products);
