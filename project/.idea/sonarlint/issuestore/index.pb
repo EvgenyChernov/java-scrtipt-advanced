9e5cc53c6628cd9f643dada4300be0554a43c436
EvgenyChernov <evgenycher89@gmail.com>
2020-07-20 09:51:17 +0300

    Задание первого урока выполнено v_02 (иначе написал функции)

--- project/.idea/sonarlint/issuestore/index.pb
+++ project/.idea/sonarlint/issuestore/index.pb
@@ -9,3 +9,7 @@ D
 styles/my-style.sass,a\3\a3b6854971f42628c33f001f2aade2cedf73e55f
 C
 styles/my-style.css,7\4\7488f1b51dd5adde4d2948e82b1053a8a06302a8
+S
+#.idea/sonarlint/issuestore/index.pb,9\f\9fe84ebb15faf917b7def6236dba604453cc61e0
+G
+styles/my-style.css.map,5\f\5f2946312995c8e3de4045afa171bc6d39501ea5
--- project/js/main.js
+++ project/js/main.js
@@ -6,14 +6,13 @@ const products = [
 ];
 
 /**
- * Функция свойства объекта  преобразует их и возвращает строку
+ * Функция получает свойства объекта и отображает карточку одного товара на странице
  * @param title название товара
  * @param price цена товара
  * @param img картинка если есть
- * @return {string} часть кода HTML (для одного продукта)
  */
 const renderProduct = (title, price, img = 'http://dummyimage.com/260x350/99cccc.jpg/0b0b0b&text=Фото+товара!') => {
-    return `<div class="product-item">
+    document.querySelector('.products').innerHTML += `<div class="product-item">
                 <h3>${title}</h3>
                 <img src="${img}" alt="">
                 <p>Цена ${price}</p>
@@ -26,12 +25,7 @@ const renderProduct = (title, price, img = 'http://dummyimage.com/260x350/99cccc
  * @param list
  */
 const renderProducts = list => {
-    const productList = list.map(item => renderProduct(item.title, item.price));
-    for (let i = 0; i < productList.length; i++) {
-        document.querySelector('.products').innerHTML += productList[i];
-    }
-    // console.log(productList);
-    // document.querySelector('.products').innerHTML = productList;
+    list.map(item => renderProduct(item.title, item.price));
 };
 
 renderProducts(products);
