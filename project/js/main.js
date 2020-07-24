eb827d1b6730956542eec9fb1a7e3cd39f745112
EvgenyChernov <evgenycher89@gmail.com>
2020-07-24 15:16:30 +0300

    Задание второго урока выполнено полностью v_02

--- project/js/main.js
+++ project/js/main.js
@@ -1,19 +1,14 @@
-window.addEventListener('load', () => {
-    const productList = new ProductList();
-
-
-    productList.fetchProducts();
-    productList.render();
-    const basket = new Basket(productList.goods);
-    basket.seeBasket();
-    basket.getButtonsBy();
-})
+"use strict";
 
 class ProductList {
-    constructor(container = '.products') {
+    constructor(container = ".products") {
         this.container = container;
         this.goods = [];
-        this.allproducts = [];
+        this.allProducts = [];
+
+        this.fetchProducts();
+        this.render();
+        new EventButtonsBy(this.goods);
     }
 
     fetchProducts() {
@@ -29,7 +24,7 @@ class ProductList {
         const block = document.querySelector(this.container);
         for (let product of this.goods) {
             const productObject = new ProductItem(product);
-            this.allproducts.push(productObject);
+            this.allProducts.push(productObject);
             block.insertAdjacentHTML('beforeend', productObject.render());
         }
     }
@@ -55,65 +50,103 @@ class ProductItem {
     }
 }
 
-class Basket {
+class EventButtonsBy {
     constructor(goods) {
-        this.id = null;
         this.goods = goods;
-        this.basketProducts = [];
+        this.eventListenersButtonsBy();
+        this.eventListenersButtonsBasket();
+
     }
 
-
     /**
-     * функция отображает поле корзины при нажатии на кнопку "корзина"
+     * метод назначает слушатель событий всем кнопкам при клике, возвращает при клике на кнопку  продукта.
      */
-    seeBasket() {
-        let basketBtns = document.querySelector(".header__button");
-        basketBtns.addEventListener("click", function () {
-            let basketList = document.querySelector(".basketList");
-            basketList.classList.add("basketList_visibility");
+    eventListenersButtonsBy = () => {
+        let arrayIdSelectedProduct = [];
+        const buttonsBy = document.querySelectorAll('.by-btn');
+        buttonsBy.forEach(buttonBy => {
+            buttonBy.addEventListener('click', event => {
+                const button = event.currentTarget;
+                const productElement = button.closest('.product-item');
+                let idProductElement = Number(productElement.dataset.id);
+                new BasketList(idProductElement, this.goods);
+            })
+
         });
     };
 
     /**
-     * Функция добавляет продукт при нажатии кнопки "Купить"
+     * метод назначает слушатель событий на кнопку корзина отображая содержимое корзины
      */
-    addToBasket(product) {
-        const basket = document.querySelector(".basketList")
-        const productBasket = new ProductItem(product);
-        this.basketProducts.push(productBasket);
-        basket.insertAdjacentHTML('beforeend', productBasket.render());
+    eventListenersButtonsBasket() {
+        let buttonStatus = 0;
+        const buttonBasket = document.querySelector('.header__button');
+        buttonBasket.addEventListener('click', function () {
+            const basketList = document.querySelector('.basketList');
+            console.log(basketList);
+            if (buttonStatus === 0) {
+                basketList.classList.add('basketList_visibility');
+                buttonStatus++;
+                return
+            }
+            basketList.classList.remove('basketList_visibility');
+            buttonStatus = 0;
+        });
+    }
+}
+
+class BasketList {
+    constructor(idProductElement, goods) {
+        this.idProductElement = idProductElement;
+        this.goods = goods;
+
+        this.productObject = {};
+        this.addProductToBasket();
     }
 
-
-    /**
-     * Функция ищет все кнопки "Купить" и задает им слушатели событий при клике кнопка возвращает data-id
-     */
-    getButtonsBy() {
-        let byButtons = document.querySelectorAll(".by-btn");
-        byButtons.forEach(function (btn) {
-            btn.addEventListener('click', function (eventClick) {
-                let elemEventClick = eventClick.currentTarget;
-                let elemDivProduct = elemEventClick.closest('.product-item')
-                let id = Number(elemDivProduct.dataset.id);
-                let goods = [
-                    {id: 1, title: 'Notebook', price: 20000},
-                    {id: 2, title: 'Mouse', price: 1500},
-                    {id: 3, title: 'Keyboard', price: 5000},
-                    {id: 4, title: 'Gamepad', price: 4500},
-                ]//TODO; не понимаю как получить массив объектов с товарами из другого класса , для дальнейшего использования, (goods)
-                for (let product of goods) {
-                    if (id === product.id) {
-                        addToBasket(product)
-                        console.log(product.title)
-
-                        //TODO по моей логике после удачного сравнения можно объект товара перенести в div корзины и создать переменную в которой
-                        // будет храниться массив объектов которые добавили в корзину
-                        // после чего циклом for перебрать и получить сумму товаров
-                    }
-                }
-            });
+    addProductToBasket = () => {
+        const basketList = document.querySelector('.basketList');
+        this.goods.forEach(good => {
+            if (this.idProductElement === good.id) {
+                this.productObject = good;
+                this.renderTotalPrice();
+                const productObjectToBasket = new ProductItemInToBasket(good);
+                basketList.insertAdjacentHTML('beforeend', productObjectToBasket.render());
+            }
         })
+    }
+
+    renderTotalPrice = () => {
+        const totalPriseInBasket = document.querySelector('.basketList__total');
+        totalPriseInBasket.textContent = "";
+        BasketList.totalPrice += Number(this.productObject.price);
+        console.log(BasketList.totalPrice);
+
+        totalPriseInBasket.insertAdjacentText('beforeend', `${BasketList.totalPrice}`)
+    };
+
+    static totalPrice = 0;
+}
+
+class ProductItemInToBasket {
+    constructor(product, img = 'http://placehold.it/100x75') {
+        this.title = product.title;
+        this.price = product.price;
+        this.id = product.id;
+        this.img = img;
+    }
+
+    render() {
+        return `<div class="basketList__product-item" data-id="${this.id}">
+                    <img class="basketList__img" src="${this.img}" alt="Something img">
+                    <div class="basketList__description">
+                        <h3>${this.title}</h3>
+                        <p>${this.price} \u20bd</p>
+                        <button class="">удалить</button>
+                    </div>
+                </div>`;
     }
 }
 
 
+new ProductList();
